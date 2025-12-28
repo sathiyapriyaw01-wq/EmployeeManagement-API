9e16f735902897669bdc4047d7245cfabb79548b
Sathiya <sathiyapriyaw01@gmail.com>
2025-12-28 22:51:21 +0400

    Added Docs folder

--- EmployeeManagement.BusinessLayer/DTOs/EmployeeDTO.cs
+++ EmployeeManagement.BusinessLayer/DTOs/EmployeeDTO.cs
@@ -6,7 +6,6 @@ namespace EmployeeManagement.BusinessLayer.DTOs
 {
     public class EmployeeDTO
     {
-        public int EmpId { get; set; }
         public string Name { get; set; }
         public string Department { get; set; }
         public string Gmail { get; set; }
